959863f763bd144db5fe6eb3a07b52a4316ec026
MdeGr <mereldegraauw@gmail.com>
2024-08-02 12:41:13 +0200

    start of basic structure

---
+++ ttrpg-combat-engine/creature/Creature.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ttrpg_combat_engine.stat;
+using ttrpg_combat_engine.attack;
+using System.ComponentModel.DataAnnotations;
+
+namespace ttrpg_combat_engine.mobile
+{
+    public class Creature
+    {
+        private List<Stat> stats;
+        private List<Attack> attacks;
+
+        public List<Stat> GetStats() {  return stats; }
+        public List<Attack> GetAttacks() { return attacks; }
+
+        internal Creature(List<Stat> stats, List<Attack> attacks)
+        {
+            this.stats = stats;
+            this.attacks = attacks;
+        }
+        internal bool Addstat(Stat stat)
+        {
+            if (stat == null) { return false; }
+            try
+            {
+                stats.Add(stat);
+                return true;
+            }
+            catch { return false; }
+        }
+        internal bool AddAttack(Attack attack)
+        {
+            if (attack == null) { return false; }
+            try
+            {
+                attacks.Add(attack);
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public bool EditStat(string statname, int curentValue)
+        {
+            if (statname == null || statname == "") { return false; }
+            try
+            {
+                foreach (Stat stat in stats)
+                {
+                    if (stat.GetName() == statname)
+                    {
+                        stat.EditCurentValue(curentValue);
+                        return true;
+                    }
+                }
+            }
+            catch { return false; }
+        }
+    }
+}
---
+++ ttrpg-combat-engine/stat/Stat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ttrpg_combat_engine.stat
+{
+    public class Stat
+    {
+        private string name;
+        private int normalValue;
+        private int curentValue;
+
+        public string GetName() {  return name; }
+        public int GetNormalValue() { return normalValue; }
+        public int GetCurentValue() { return curentValue; }
+
+        internal Stat(string name, int normalValue)
+        {
+            this.name = name;
+            this.normalValue = normalValue;
+        }
+        internal bool EditCurentValue(int curentValue)
+        {
+            try
+            {
+                this.curentValue = curentValue;
+                return true;
+            }
+            catch { return false; }
+        }
+    }
+}
