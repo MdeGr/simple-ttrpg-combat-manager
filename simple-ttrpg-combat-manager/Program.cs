406b93f0ec1fff98cd5b81640af04414f5961d75
MdeGr <mereldegraauw@gmail.com>
2025-01-26 23:14:22 +0100

    progress

--- simple-ttrpg-combat-manager/Program.cs
+++ simple-ttrpg-combat-manager/Program.cs
@@ -1,5 +1,34 @@
 ﻿using simple_ttrpg_combat_manager.UI;
+using consoleFrontendFramework.UiPreset;
+using consoleFrontendFramework.interfaces;
+using consoleFrontendFramework.ActionPreset;
+using simple_ttrpg_combat_manager.UI.screens.Create;
+using simple_ttrpg_combat_manager.UI.screens.load;
 
-UIManager uiManager = new UIManager();
-uiManager.run();
+//initiolizing start screen
+string header = "Simple combat manager\n" +
+            " by: Merel de Graauw\n\n";
+NavigatingUI startscreen = new NavigatingUI(header);
+
+IAction[] actions= [
+new NavigateTo("Create encounter",new CreateEncounter()),
+new NavigateTo("Load encounter", new LoadEncounter()),
+new ExitAction(startscreen)];
+
+startscreen.setActions(actions);
+
+//initiolize UI manager
+UIManager uiManager = new UIManager(startscreen);
+
+//first loading of UI
+Console.WriteLine(uiManager.RunCycle(""));
+//core running loop
+while (uiManager.GetRunning() == true)
+{
+    string? input = Console.ReadLine();
+    Console.Clear();
+    string output = uiManager.RunCycle(input);
+    Console.WriteLine(output);
+}
+
 Console.WriteLine("The aplication has stopped");
